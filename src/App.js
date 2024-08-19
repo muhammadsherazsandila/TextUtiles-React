fb353804885825d341319152f7db4e31d07588d6
muhammad sheraz <msd.sheraz046@gmail.com>
2024-08-19 22:00:30 +0500

    adding bootstrap and understanding of props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar bg-body-secondary">
+        <div className="container-fluid">
+          <h3 className="navbar-brand">Navbar</h3>
+          <form className="d-flex" role="search">           
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-primary" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </>
   );
 }
 
